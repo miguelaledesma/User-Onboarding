0f4cadcc77ab5975d68419a74bf3c8e49217a6b6
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-02-23 12:50:52 -0800

    created form in Forms.js

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,9 +1,27 @@
+import React, { useState } from 'react';
+
 import './App.css';
+import Forms from './Components/Forms';
+
+const initialFormValues = {
+  username: '', 
+  password: '',
+  email: '',
+  checked: false
+
+}
+
 
 function App() {
+
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+
+
+
   return (
     <div className="App">
-      
+      <Forms /> 
     </div>
   );
 }
---
+++ user-onboarding/src/Components/Forms.js
@@ -0,0 +1,75 @@
+import React from 'react'
+
+const Forms = (props) => {
+const { change, submit } = props 
+
+const {username, email, password, checked } = props.values 
+
+const onChange = (evt) => {
+    const { name, value, type, checked} = evt.target
+
+    const valueToUse = type === 'checkbox' ? checked : value ; 
+    change(name, valueToUse)
+
+}
+
+const onSubmit = evt => {
+    evt.preventDefault()
+    submit()
+}
+
+
+return (
+    <div> 
+    <h1> My Form </h1>
+    <form onSubmit = {onSubmit}> 
+        <label> Name: 
+            <input 
+            name = 'username'
+            type = 'text'
+            value = {username}
+            onChange = {onChange}
+            
+            /> 
+        </label>
+        <label> Email: 
+            <input
+            name ='email'
+            type = 'text'
+            value ={email}
+            onChange = {onChange}
+            
+            /> 
+        </label>
+    <label> Password: 
+
+        <input 
+        
+        name = 'password'
+        type = 'password'
+        value = {password}
+        onChange = {onChange}
+        
+        /> 
+
+    </label>
+
+    <label> Terms of Service
+        <input 
+        type = 'checkbox'
+        name = 'TOS'
+        checked = {checked}
+        onChange = {onChange}
+        
+        /> 
+
+    </label>
+
+    <input type = 'submit' value = 'Create a new User' /> 
+
+    </form>
+    </div>
+)
+}
+
+export default Forms 
