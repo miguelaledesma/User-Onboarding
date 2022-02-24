8c28817d30ab37f2314c24f09749107aee4b1f5a
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-02-24 12:36:51 -0800

    got a sanity check test to pass

---
+++ user-onboarding/cypress/integration/form_test.js
@@ -0,0 +1,16 @@
+//writing my tests here
+
+describe("Testing form inputs ", ()=>{  
+
+   
+    beforeEach(() =>{
+        cy.visit('http://localhost:3000/')
+
+    })
+    
+    it('sanity checks', () => {
+        
+    })
+
+
+})
