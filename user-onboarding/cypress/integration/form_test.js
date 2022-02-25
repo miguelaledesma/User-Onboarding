c396d65ec04fe3da07168ff48c6cdcd2391fd6a0
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-02-24 16:43:07 -0800

    testing done

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -7,11 +7,21 @@ describe("User Onboarding ", ()=>{
         cy.visit('http://localhost:3000/')
 
     })
-    
+
+    const textInput = () => cy.get("input[type=text]")
+    const emailInput = () => cy.get("input[type=text]")
+    const passwordInput = () => cy.get("input[type=password]")
+    const tosInput = () => cy.get("input[type=checkbox]")
+
     it('sanity checks', () => {
         //assertion goes here
         expect(5).to.equal(5)
     })
-
+    it('checks if input is showing', () => {
+        textInput().should('exist')
+        emailInput().should('exist')
+        passwordInput().should('exist')
+        tosInput().should('exist')
+    })
 
 })
